e0f2332ef3f412b78341d26f9c4cda374b728636
Mihai Galos <mihaigalos@gmail.com>
2019-06-22 11:07:27 +0200

    Update invokation

--- i3wm_wrapper.sh
+++ i3wm_wrapper.sh
@@ -3,5 +3,5 @@
 i3status -c /etc/i3status.conf | while :
 do
     read line
-    echo $line | python /home/mihai/git/i3status-provider/i3wm_wrapper.py /home/mihai/.netatmo-credentials.yaml || exit 1
+    echo $line | python /home/mihai/git/i3status-provider/i3wm_wrapper.py || exit 1
 done
