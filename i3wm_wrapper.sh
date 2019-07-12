424ea02617bfcb72bc601a296d71a882155dafb2
Mihai Galos <mihai.galos@bmw.de>
2019-07-12 15:23:57 +0200

    Source zshrc to make provider work behind a proxy

--- i3wm_wrapper.sh
+++ i3wm_wrapper.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+source ~/.zshrc
+
 i3status -c /etc/i3status.conf | while :
 do
     read line
-    echo $line | python /home/mihai/git/i3status-provider/i3wm_wrapper.py || exit 1
+    echo $line | python -u ~/git/i3status-provider/i3wm_wrapper.py || exit 1
 done
