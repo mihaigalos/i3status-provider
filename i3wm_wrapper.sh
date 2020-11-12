fa74979834ab9477c076b1cebba3734f8951f888
Mihai Galos <mihaigalos@gmail.com>
2020-11-12 16:29:35 +0100

    Explicitly call python3

--- i3wm_wrapper.sh
+++ i3wm_wrapper.sh
@@ -5,5 +5,5 @@ source ~/.zshrc
 i3status -c /etc/i3status.conf | while :
 do
     read line
-    echo $line | python -u ~/git/i3status-provider/i3wm_wrapper.py || exit 1
+    echo $line | python3 -u ~/git/i3status-provider/i3wm_wrapper.py || exit 1
 done
